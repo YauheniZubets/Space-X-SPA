75e748ddd3041f74896975bdfa9fca701ce04767
Yauheni Zubets <gunner17djg@gmail.com>
2021-07-06 22:19:29 +0300

    spaceX v1.0

--- __tests__/getUserAge-test.js
+++ __tests__/getUserAge-test.js
@@ -4,10 +4,10 @@ import {getAge} from '../components/Modalreg/modules/getUserAge';
 
 test('проверка получения возраста пользователя по дате рождения', () => {
 
-  expect(getAge('1993/05/17')).toBe(27);
+  expect(getAge('1993/05/17')).toBe(28);
 
-  expect(getAge('2020/05/17')).toBe(0);
+  expect(getAge('2020/05/17')).toBe(1);
 
-  expect(getAge('1979/05/17')).toBe(41);
+  expect(getAge('1979/05/17')).toBe(42);
 
 });
